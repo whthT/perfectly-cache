63221aef7df976e22b437542fd4aff06e7a9d624
Musa Kurt <hackedtim61@gmail.com>
2019-01-28 19:03:59 +0300

    Package Init

---
+++ src/Facade/PerfectlyCache.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Musa
+ * Date: 28.01.2019
+ * Time: 18:17
+ */
+
+namespace Whtht\PerfectlyCache\Facade;
+
+
+class PerfectlyCache
+{
+
+}
---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Musa
+ * Date: 28.01.2019
+ * Time: 18:19
+ */
