b0afaedcff22d7d72ff1f311f9eb0ac829ce9f19
Musa Kurt <hackedtim61@gmail.com>
2019-03-25 17:57:01 +0300

    List command

---
+++ src/Commands/PerfectlyCacheListCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Musa
+ * Date: 25.03.2019
+ * Time: 17:26
+ */
+
+namespace Whtht\PerfectlyCache\Commands;
+
+
+class PerfectlyCacheListCommand
+{
+
+}
